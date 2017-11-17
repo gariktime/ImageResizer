08f9fa581ffd36881a89bc9b7a40bb6b13c92f65
gariktime <maximgagarinov93@gmail.com>
2017-11-17 18:18:41 +0000

    OpenFileDialog and stuff

--- ImageResizer/MainWindow.xaml.cs
+++ ImageResizer/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,41 @@ namespace ImageResizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string resizePath = string.Empty;
+        private int maxFileSize = 5000;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        //кнопка выбора пути
+        private void btnOpenPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.AddToMostRecentlyUsedList = false;
+            dialog.AllowNonFileSystemItems = false;
+            dialog.EnsureFileExists = true;
+            dialog.EnsurePathExists = true;
+            dialog.EnsureReadOnly = false;
+            dialog.EnsureValidNames = true;
+            dialog.Multiselect = false;
+            dialog.ShowPlacesList = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txbPath.Text = dialog.FileName;
+                resizePath = dialog.FileName;
+            }
+        }
+
+        private void btnResize_Click(object sender, RoutedEventArgs e)
+        {
+            string[] filePaths = Directory.GetFiles(resizePath, "*.jp*", SearchOption.AllDirectories); //список файлов всех изображений
+            List<string> resizeableFiles = ResizeMethods.GetResizeableFiles(filePaths, maxFileSize); //список файлов, подлежащих ресайзу
+        }
+
+
     }
 }
---
+++ ImageResizer/Utility/ResizeMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    public static class ResizeMethods
+    {
+        /// <summary>
+        /// Возвращает список полных имён файлов 
+        /// </summary>
+        /// <param name="filePaths"></param>
+        /// <param name="maxFileSize"></param>
+        /// <returns></returns>
+        public static List<string> GetResizeableFiles(string[] filePaths, int maxFileSize)
+        {
+            List<string> result = new List<string>();
+
+            Parallel.ForEach(filePaths, (currentFilePath) =>
+            {
+                FileInfo fileInfo = new FileInfo(currentFilePath);
+                //если размер файла больше заданного, то добавляем в List
+                if (fileInfo.Length / 1024 > maxFileSize)
+                    result.Add(currentFilePath);
+            });
+
+            return result;
+        }
+
+        public static void ResizeStandart(List<string> filePaths)
+        {
+            Parallel.ForEach(filePaths, (currentFilePath) =>
+            {
+                ResizeStandart(currentFilePath);
+            });
+        }
+
+        public static void ResizeStandart(string filePath)
+        {
+
+        }
+    }
+}
